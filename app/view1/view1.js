f17e5f8e496c2ed908411cad88479d36e4ddb6e9
Niklas Larsson <niklaslarsson95@gmail.com>
2017-02-01 19:49:12 +1100

    First stages of data input form

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,35 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', function($scope) {
 
+  $scope.courses = [];
+  $scope.classes = [];
+  $scope.class_times = [];
+
+  $scope.addCourse = function() {
+      var result = {
+          name: $scope.course_name,
+          classes: $scope.classes
+      };
+
+      $scope.courses.push(result);
+  };
+
+  $scope.addClass = function() {
+      var result = {
+          name: $scope.class_name,
+          times: $scope.class_times
+      };
+
+      $scope.classes.add(result);
+  };
+
+  $scope.addTime = function() {
+      var result = {
+          time: $scope.time
+      };
+
+      $scope.class_times.add(result);
+  }
 }]);
