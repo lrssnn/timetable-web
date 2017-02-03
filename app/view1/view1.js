0a6209195846e3d59d986677ae6133eb439aa835
Niklas Larsson <niklaslarsson95@gmail.com>
2017-02-03 16:51:39 +1100

    Improve Day/time model to set up for event display

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,42 +2,60 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.controller('View1Ctrl', ['$scope', 'Courses', function($scope, Courses) {
+    .controller('View1Ctrl', ['$scope', 'Courses', function ($scope, Courses) {
 
-    $scope.courses = Courses;
-  $scope.addCourse = function() {
-      var result = {
-          name: $scope.course_name,
-          classes: []
-      };
+        $scope.courses = Courses;
+        $scope.addCourse = function () {
+            var result = {
+                name: $scope.course_name,
+                classes: []
+            };
 
-      Courses.push(result);
-  };
+            Courses.push(result);
+        };
 
-  $scope.add_class_to_course = function(course) {
-      course.classes.push({
-          name: course.new_text,
-          dur: course.new_dur,
-          options: []
-      })
-  }
+        $scope.add_class_to_course = function (course) {
+            course.classes.push({
+                name: course.new_text,
+                dur: course.new_dur,
+                options: []
+            })
+        }
 
-  // Adopting 'clss' as 'class' because class is a keyword
-  $scope.add_option_to_class = function(clss) {
-      clss.options.push({
-          day: clss.new_day,
-          time: clss.new_time
-      })
-  }
+        // Adopting 'clss' as 'class' because class is a keyword
+        $scope.add_option_to_class = function (clss) {
+            clss.options.push({
+                day: clss.new_day,
+                time: clss.new_time
+            })
+        }
 
-  $scope.time_string_from_option = function(option) {
-      return "IMDABES"
-  }
-}]);
+        $scope.time_string_from_option = function (option) {
+            return "IMDABES";
+        }
+
+        $scope.time_range = function () {
+            return ["08:00", "08:30", "09:00", "09:30", "10:00", "10:30",
+                    "11:00", "11:30", "12:00", "12:30", "13:00", "13:30",
+                    "14:00", "14:30", "15:00", "15:30", "16:00", "16:30",
+                    "17:00", "17:30", "18:00", "18:30", "19:00", "19:30"];
+        }
+
+        $scope.day_range = function(){
+            var res = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
+            var weekend = true;
+            if(weekend){
+                return res.concat(['Saturday', 'Sunday']);
+            }
+
+            return res;
+        }
+    }]);
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -11,4 +11,22 @@ angular.module('myApp.view2', ['ngRoute'])
 
 .controller('View2Ctrl', ['$scope', 'Courses', function($scope, Courses) {
     $scope.courses = Courses;
+
+    $scope.time_range = function () {
+        return ["08:00", "08:30", "09:00", "09:30", "10:00", "10:30",
+            "11:00", "11:30", "12:00", "12:30", "13:00", "13:30",
+            "14:00", "14:30", "15:00", "15:30", "16:00", "16:30",
+            "17:00", "17:30", "18:00", "18:30", "19:00", "19:30"];
+    }
+
+    $scope.day_range = function(){
+        var res = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
+        var weekend = true;
+        if(weekend){
+            return res.concat(['Saturday', 'Sunday']);
+        }
+
+        return res;
+    }
 }]);
