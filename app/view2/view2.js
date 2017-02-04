46c3d39bffef2f99a41be754a8f12ce7c0a00dc1
Niklas Larsson <niklaslarsson95@gmail.com>
2017-02-04 18:22:36 +1100

    Begin working on placing options in the timetable
    Iteration code is very ugly, but there appears to be no good array
    enumeration in JS

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -17,7 +17,7 @@ angular.module('myApp.view2', ['ngRoute'])
             "11:00", "11:30", "12:00", "12:30", "13:00", "13:30",
             "14:00", "14:30", "15:00", "15:30", "16:00", "16:30",
             "17:00", "17:30", "18:00", "18:30", "19:00", "19:30"];
-    }
+    };
 
     $scope.day_range = function(){
         var res = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
@@ -25,6 +25,29 @@ angular.module('myApp.view2', ['ngRoute'])
         var weekend = true;
         if(weekend){
             return res.concat(['Saturday', 'Sunday']);
+        }
+
+        return res;
+    };
+
+    $scope.options_in_timeslot = function(day, time) {
+        var res = [];
+
+
+
+        for(var i in Courses){
+            var course = Courses[i];
+            for(var j in course.classes){
+                var clss = course.classes[j];
+                for(var k in clss.options) {
+                    var opt = clss.options[k];
+                    if (opt.day == day && opt.time == time) {
+                        console.log(day, time);
+                        console.log(course);
+                        res.push(opt);
+                    }
+                }
+            }
         }
 
         return res;
