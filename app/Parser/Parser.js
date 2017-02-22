f5c82f7d500bfd61b850145d09dbde3bea5270fc
Niklas Larsson <niklaslarsson95@gmail.com>
2017-02-22 14:30:56 +1100

    Comments, error management, result display for course parsing

--- app/Parser/Parser.js
+++ app/Parser/Parser.js
@@ -12,42 +12,58 @@ angular.module('Timetables.Parser', ['ngRoute'])
 
     $scope.courses = Courses;
 
+    // Error wrapper for parse_course_from_html. Adds the resultant course to the global array if successful.
     $scope.create_course_from_html = function(){
-        var raw_text = $scope.raw_text;
+        var course;
+        try {
+            course = parse_course_from_html();
+        } catch (e){
+            $scope.parse_result = "Parsing error occurred. Ensure you are copying the full source of the correct page." +
+                "\nIf error persists contact me enter the data manually and contact me about the error." +
+                "\nFull error Text: '" + e + "'";
+            return;
+        }
+        $scope.parse_result = "Parsing Complete. View data in timetable or add another course.";
+        if(course) $scope.courses.push(course);
+        $scope.raw_text = "";
+    };
+
+    // Takes the html source of a uon timetable search result and returns a course created from that data
+    function parse_course_from_html() {
+        var raw_text = $scope.raw_text; // Full page source
 
         // Create the parser and parse the text
         var handler = new Tautologistics.NodeHtmlParser.DefaultHandler(function (error, dom) {
             if (error){
-                console.log("ERROR");
-            } else {
-                console.log("Success?");
+                console.log("Parsing error occurred: ", error);
             }
         }, {ignoreWhitespace: true, verbose:false});
-
         var parser = new Tautologistics.NodeHtmlParser.Parser(handler);
         parser.parseComplete(raw_text);
 
-        var dom = handler.dom;
-        var table_tag = find_table_tag(dom);
-        console.log(table_tag);
+        // Searches the dom for the data table's root tag from where we begin pulling out the data
+        var table_tag = find_table_tag(handler.dom);
 
-        // List of all options parsed from the table. This list will get the first step of parsing, before each option
-        // is resolved into its resultant course.
+        // Build a list of "flat options" which are objects built more or less from the data in one row of the column.
+        // These flat options contain the information necessary to build the class->opt hierarchy but this is done later
         var flat_opts = [];
-        var course_name = "";
+        var course_name = ""; // This will be populated later
 
+        // The table consists of a head, body and foot, pull out the body
         for(var i in table_tag.children){
             if(table_tag.children[i].name == "tbody"){
                 var tab_body = table_tag.children[i];
+                // Traverse each row in the column, and pull its children which is each column in the table.
                 for(var j in tab_body.children){
                     var row = tab_body.children[j];
-                    console.log(row);
                     var cols = row.children;
+                    // Each set of columns is well defined and ordered so we can explicitly pull the data we need.
                     var class_code = cols[0].children[0].children[0].data;
                     var class_name = cols[1].children[0].data;
                     var day = cols[2].children[1].data;
                     var start_time = cols[3].children[1].data;
                     var duration = cols[5].children[1].data;
+                    // Build the option and store it
                     var flat_option = {
                         class_code: class_code,
                         class_name: class_name,
@@ -60,29 +76,28 @@ angular.module('Timetables.Parser', ['ngRoute'])
             }
         }
 
-        console.log(flat_opts);
-
         // Now we need to determine the class/option relationship by reading the class code of each option
         var classes = [];
         for(var i in flat_opts){
             var opt = flat_opts[i];
-            var parent_class = null;
+            var parent_class = null; // This will reference the option's parent in the classes array once found/created
             var class_details = parse_course_code(opt.class_code);
+            // Get the course name
             if(!course_name){
                 course_name = class_details.course_name;
             }
+            // For each class we know about, check if the class matches this option's class
             for(var j in classes){
                 var clss = classes[j];
-                console.log(clss, class_details);
                 if(clss.id == class_details.class_id){
                     // This is an option for an existing class, set this class as the target
                     parent_class = clss;
                     break;
                 }
             }
-
+            // If the option doesn't match any existing classes, add a new one
             if(parent_class == null){
-                // This option belongs to a class we don't have yet, add it to the list
+
                 var clss = {
                     name: opt.class_name,
                     dur: parseInt(opt.dur.split()[0]),
@@ -101,15 +116,14 @@ angular.module('Timetables.Parser', ['ngRoute'])
                 id: parseInt(class_details.option_id),
             });
         }
-
-        console.log(classes);
-        $scope.courses.push({
+         // Add a new course with our created data
+        return {
             name: course_name,
             colour: get_colour.next(),
             active: true,
             collapsed: false,
             classes: classes
-        })
+        }
     };
 
     function find_table_tag(children){
